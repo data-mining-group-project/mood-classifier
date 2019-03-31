fd02ede01b84f17aaa1ab1d27fbf3b324cbcbb30
Guiwald <gwxtrash@gmail.com>
2019-03-31 23:59:47 +0100

    Update getTracksList.R

--- getTracksList.R
+++ getTracksList.R
@@ -1,9 +1,15 @@
-# Change the following parameters 
+#################################################
+## This script return all the url of tracks of ##
+## a playlist of more than 100 tracks          ##
+#################################################
+
+
+## Change the following parameters 
 playlistID <- "5sjzvvlQ5CYz0b7d36Wroz"
 playlistSize <- 280
 
 
-
+## Looping to get all the songs
 i <- 1
 getOffset <- 0
 playlistTrack <- ''
@@ -14,25 +20,8 @@ for (i in 0:((playlistSize-1) %/% 100)) {
                                          include_meta_info = FALSE)
   playlistTrackTrack <- playlistTrackTemp$track.external_urls.spotify
   playlistTrack <- rbind(data.frame(playlistTrackTrack), playlistTrack)
-  Sys.sleep(1)
+  #Sys.sleep(1)
 }
 
 playlistTrack
 
-
-
-for (user in 1:nrow(bdims)) {
-  print(paste("next year you'll be ", bdims$age[user] + 1))
-}
-
-#for (i in 1:10) {
-#  print(paste("next year you'll be ", bdims$age[i] + 1))
-#}
-
-
-for (user in 100:110) {
-  if (bdims$age[user] <25){
-    print("you're under 25!!")
-  } else { print("OK you're over 25")}
-  
-}
