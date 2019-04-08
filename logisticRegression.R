e3ad64ba5b61868a7fadc6feb9495bfab2859357
Guiwald <guiwald@gmail.com>
2019-04-08 18:22:01 +0100

    Create 2 logistic regression tries
    Created 2 logistic regression script with 2 different methods to test them.

--- logisticRegression.R
+++ logisticRegression.R
@@ -1,19 +1,50 @@
 
-
-# subsetting to only useful columns
-
-features <- songFeatures[-c(1,2)]
-
 # Examine the dataset to identify potential independent variables
-str(features)
+str(reducedSongFeatures)
+
+# removing lines with NA
+library(tidyr)
+reducedSongFeatures <- reducedSongFeatures %>% drop_na()
 
 # Explore the dependent variable
-table(features$label)
+table(reducedSongFeatures$label)
 
 # Build the donation model
-moodModel <- glm(label ~ danceability + energy + key + loudness + mode + speechiness + acousticness + 
+moodModel <- glm(label ~ danceability + key + loudness + mode + speechiness + 
                    instrumentalness + liveness + valence + tempo + duration_ms, 
-                         data = features, family = "binomial")
+                         data = reducedSongFeatures, family = "binomial")
 
 # Summarize the model results
 summary(moodModel)
+str(moodModel)
+test <- predict(moodModel, type = "response")
+str(test)
+
+# Estimate the happiness probability
+reducedSongFeatures$moodProb <- predict(moodModel, type = "response")
+
+# Find the donation probability of the average prospect
+mean(reducedSongFeatures$label)
+
+# Predict a donation if probability of donation is greater than average
+reducedSongFeatures$moodPred <- ifelse(reducedSongFeatures$moodProb > 0.4225, 1, 0)
+
+# Calculate the model's accuracy
+mean(reducedSongFeatures$label == reducedSongFeatures$moodPred)
+# [1] 0.746842
+
+## CALCULATING ROC CURVE AND AUC
+
+# Load the pROC package
+install.packages("pROC")
+library(pROC)
+
+# Create a ROC curve
+ROC <- roc(reducedSongFeatures$label, reducedSongFeatures$moodPred)
+
+# Plot the ROC curve
+plot(ROC, col = "blue")
+
+# Calculate the area under the curve (AUC)
+auc(ROC)
+#Area under the curve: 0.7445
---
+++ logisticRegression2.R
@@ -0,0 +1,42 @@
+library(caret)
+
+
+# Removing NAs
+reducedSongFeatures <- reducedSongFeatures %>% drop_na()
+
+
+# We need to change label as a 2 level factor, or we get the following warning message: 
+# Warning message:
+# In train.default(x, y, weights = w, ...) :
+#  You are trying to do regression and your outcome only has two possible values 
+# Are you trying to do classification? If so, use a 2 level factor as your outcome column.
+
+reducedSongFeatures$label <- as.factor(reducedSongFeatures$label)
+
+
+
+Train <- createDataPartition(reducedSongFeatures$label, p = 0.8, list = FALSE) 
+training <- reducedSongFeatures[Train, ]
+testing <- reducedSongFeatures[-Train, ]
+mod_fit <- train(label ~ danceability + key + loudness + mode + speechiness + instrumentalness + 
+                   liveness + valence + tempo + duration_ms, data = training, method = "glm",
+                 family = "binomial")
+predictions <- predict(mod_fit, testing[, -11])
+table(predictions, testing[, 11])
+
+# p = 0.8 -> 74.60 % correct
+# predictions    0    1
+#           0 1461  468
+#           1  312  830
+
+# p = 0.8 -> 74.00 % correct
+# predictions   0   1
+#           0 728 241
+#           1 158 408
+
+
+
+
+
+
+
