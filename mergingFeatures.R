2987a4daf63cb8d8eb6ed984781ca1485fe1464a
Guiwald <guiwald@gmail.com>
2019-04-19 19:40:04 +0100

    update corAnalysis.R
    - Tidied R files: removing NAs in mergingFeatures.R instead of logisticRegression.R
    - adding needed libraries
    - adding Box plot and Density Cures
    - adding access_token in prediction.R .

--- mergingFeatures.R
+++ mergingFeatures.R
@@ -6,4 +6,18 @@ sad <- read.csv("sad_variables.csv")
 # Merging
 songFeatures <- rbind(happy, sad)
 
+# Removing NAs
+
+library(tidyr)
+songFeatures <- songFeatures %>% drop_na()
+
+
+# We need to change label as a 2 level factor, or we get the following warning message: 
+# Warning message:
+# In train.default(x, y, weights = w, ...) :
+#  You are trying to do regression and your outcome only has two possible values 
+# Are you trying to do classification? If so, use a 2 level factor as your outcome column.
+
+songFeatures$label <- as.factor(songFeatures$label)
+
 write.csv(songFeatures, file = "songFeatures.csv")
--- prediction.R
+++ prediction.R
@@ -1,6 +1,11 @@
 ## Trying your own song by entering song URL or URI
 ## Example URL https://open.spotify.com/track/2acK24b60RQD2zBpW0Zsrw?si=HYTW4a8LSVSoaMmomlbaUA
 ## Example URI spotify:track:2acK24b60RQD2zBpW0Zsrw
+library(spotifyr)
+Sys.setenv(SPOTIFY_CLIENT_ID = '99a73bb020fa434c9dd297c35c45d12d')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = 'fd31c8e400c448e1bf6e7d90edef915b')
+
+access_token <- get_spotify_access_token()
 
 # Loading models
 modLR <- readRDS(file = "files/mod_log_regression.rds")
@@ -17,8 +22,8 @@ songTitle <- get_track(songID)$name
 songArtist <- get_track(songID)$artists$name
 
 songMoodPredictLR <- predict(modLR, get_track_audio_features(songID)[-c(12:16, 18, hc)])
-songMoodPredictNN <- predict(nnetMood, get_track_audio_features(songID)[-c(12:16, 18, hc)])
-songMoodPredictMars <- predict(marsMod, get_track_audio_features(songID)[-c(12:16, 18, hc)])
+# songMoodPredictNN <- predict(nnetMood, get_track_audio_features(songID)[-c(12:16, 18, hc)])
+# songMoodPredictMars <- predict(marsMod, get_track_audio_features(songID)[-c(12:16, 18, hc)])
 
 library(randomForest)
 songMoodPredictRF <- predict(modRF, newdata = get_track_audio_features(songID)[-c(12:16, 18)] , 
@@ -30,11 +35,11 @@ if(songMoodPredictLR == 1){
   mood <- "sad"
 }
 
-if(songMoodPredictNN == 1){
-  moodNN <- "happy"
-} else{
-  moodNN <- "sad"
-}
+# if(songMoodPredictNN == 1){
+#   moodNN <- "happy"
+# } else{
+#   moodNN <- "sad"
+# }
 
 if(songMoodPredictRF == 1){
   moodRF <- "happy"
@@ -42,13 +47,13 @@ if(songMoodPredictRF == 1){
   moodRF <- "sad"
 }
 
-if(songMoodPredictMars == 1){
-  moodMars <- "happy"
-} else{
-  moodMars <- "sad"
-}
+# if(songMoodPredictMars == 1){
+#   moodMars <- "happy"
+# } else{
+#   moodMars <- "sad"
+# }
 
 cat("According to Logistic regression, the song '", songTitle, "' from", songArtist, "is", mood)
-cat("\nAccording to Neural Network, the song '", songTitle, "' from", songArtist, "is", moodNN)
+# cat("\nAccording to Neural Network, the song '", songTitle, "' from", songArtist, "is", moodNN)
 cat("\nAccording to Random Forest, the song '", songTitle, "' from", songArtist, "is", moodRF)
-cat("\nAccording to Multivariate Adaptive Regression Splines (MARS), the song '", songTitle, "' from", songArtist, "is", moodMars)
+# cat("\nAccording to Multivariate Adaptive Regression Splines (MARS), the song '", songTitle, "' from", songArtist, "is", moodMars)
