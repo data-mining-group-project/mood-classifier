452571fd0506907f7a05983788cb8c2c9c679621
elisabettad <49128684+elisabettad@users.noreply.github.com>
2019-04-19 19:37:46 +0100

    Update Shiny_mood.R
    Updated code to show results of both algorithms as well as song title and band name in the output

--- Shiny_mood.R
+++ Shiny_mood.R
@@ -1,22 +1,24 @@
 #shinyapp
 library(shiny)
 library(spotifyr)
+library(randomForest)
+library(tree)
+library(caret)
 ui <- fluidPage(titlePanel("Mood Classifier"),
-                
                 sidebarLayout(
-                  sidebarPanel( textInput(inputId = "song", label = "Enter Song URL"),
-                                # submit button
-                                actionButton("submit", label = "Submit")),
-                  mainPanel(textOutput("mood"))
-                )
-)
+                  sidebarPanel(textInput(inputId = "song", label = "Enter Song URL"),
+                               actionButton("submit", label = "Submit")),
+                  mainPanel(htmlOutput("moodRF"))
+                  )
+      )
 server <- function(input, output) {
   text_reactive <- observeEvent(input$submit, {
-    output$mood <- renderText({
+    output$moodRF <- renderUI({
       Sys.setenv(SPOTIFY_CLIENT_ID = '9911fb0ae7754159b26b9f120edf02f9')
       Sys.setenv(SPOTIFY_CLIENT_SECRET = 'a201b267bf23439a8f6f2dce5e77c102')
       access_token <- get_spotify_access_token()
       #modLR <- readRDS(file = "files/mod_log_regression.rds")
+      modRF <- readRDS(file = "./mod_rf.rds")
       modLR <- readRDS(file = "./mod_log_regression.rds")
       songURL <- input$song
       temp <- sub(".*:", "", songURL)
@@ -24,12 +26,20 @@ server <- function(input, output) {
       songID <- sub("\\?.*", "", temp)
       songTitle <- get_track(songID)$name
       songArtist <- get_track(songID)$artists$name
+      songMoodPredictRF <- predict(modRF, get_track_audio_features(songID)[-c(12:16, 18)])
       songMoodPredictLR <- predict(modLR, get_track_audio_features(songID)[-c(12:16, 18)])
-      if(songMoodPredictLR == 1){
-        mood <- "Happy :)"
-      } else{
-        mood <- "Sad :("
+      if(songMoodPredictRF == 1){
+        mood <- HTML(paste0("According to the Random Forest algorithm the song '", songTitle, "' from ", songArtist, " is happy", '<br/>'))
+      } else{?
+          mood <- HTML(paste0("According to the Random Forest algorithm the song '", songTitle, "' from ", songArtist, " is sad", '<br/>'))
       }
-      #cat("the song '", songTitle, "' from", songArtist, "is", mood)
-    }) })}
+      if(songMoodPredictLR == 1){
+        mood <- HTML(paste0(mood, "According to the Linear Regression algorithm the song '", songTitle, "' from ", songArtist, " is happy"))
+      } else{?
+          mood <- HTML(paste0(mood, "According to the Linear Regression algorithm the song '", songTitle, "' from ", songArtist, " is sad"))
+      }
+    })
+  }) 
+}
 shinyApp(ui = ui, server = server)
+
